238c3297956e724fc6276728c76d50a5cb58fa56
robyjuniansyah777 <32416484+robyjuniansyah777@users.noreply.github.com>
2017-10-15 08:59:54 +0700

    Add files via upload

---
+++ pertemuan 2/kuliah.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+cout <<"1. Umur6 tahun masuk kelas 1 sd"<< endl;
+
+cout <<"2. Naik kelas 2"<< endl;
+
+cout <<"3. Naik kelas 3"<< endl;
+ 
+cout <<"4. Naik kelas 4"<< endl;
+
+cout <<"5. Naik kelas 5"<< endl;
+
+cout <<"6. Naik kelas 6"<< endl;
+
+cout <<"7. Lulus sd"<< endl;
+
+cout <<"8. Umur 12"<< endl;
+
+cout <<"9. Masuk smp kelas 1"<< endl;
+
+cout <<"9. Naik kelas 2"<< endl;
+
+cout <<"10. Naik kelas 3"<< endl;
+
+cout <<"11. lULUS SMP"<< endl;
+
+cout <<"12. Umur 15 tahun masuk smk kelas 1"<< endl;
+
+cout <<"13. Naik kelas 1"<< endl;
+
+cout <<"14. Naik kelas 2"<< endl;
+
+cout <<"15. Naik kelas 3"<< endl;
+
+cout <<"16. Lulus smk"<< endl;
+
+cout <<"17. Umur 18 tahun masuk universitas"<< endl;
+
+cout <<"18. Semester 1"<< endl;
+
+cout <<"19. Semester 2"<< endl;
+
+cout <<"20. Semester 3"<< endl;
+
+cout <<"21. Semester 4"<< endl;
+
+cout <<"22. Semester 5"<< endl;
+
+cout <<"23. Semester 6"<< endl;
+
+cout <<"24. Semester 7"<< endl;
+
+cout <<"25. Semester 8"<< endl;
+
+cout <<"26. Lulus menjadi sarjana"<< endl;
+
+
+return 0;
+
+}
