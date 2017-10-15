bf454eec5f6a21146f7ac5a8f0eda63239f708b5
robyjuniansyah777 <32416484+robyjuniansyah777@users.noreply.github.com>
2017-10-15 09:02:51 +0700

    Add files via upload

---
+++ pertemuan 2/modular.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+void prosedursd();
+void prosedursmp();
+void prosedursmk();
+void prosedurkuliah();
+
+return 0;
+
+}
+
+
+void prosedursd()
+
+{
+
+cout <<"1.Umur 6 tahun maasuk kelas 1"<< endl;
+ 
+cout <<"2.Naik kelas 2"<< endl;
+
+cout <<"3.Naik kelas 3"<< endl;
+
+cout <<"4.Naik kelas 4"<< endl;
+
+cout <<"5.Naik kelas 5"<< endl;
+
+cout <<"6.Naik kelas 6"<< endl;
+
+cout <<"7.Lulus sd"<< endl;
+
+}
+
+void prosedursmp()
+
+{
+
+cout <<"8.Umur 12 tahun masuk smp kelas 1"<< endl;
+
+cout <<"9.Naik kelas 2"<< endl;
+
+cout <<"10.Naik kelas 3"<< endl;
+
+cout <<"11.Lulus smp"<< endl;
+
+}
+
+void prosedursmk()
+
+{
+
+cout <<"12.Umur 15 tahun masuk smk kelas 1"<< endl;
+
+cout <<"13.Naik kelas 2"<< endl;
+
+cout <<"14.Naik kelas 3"<< endl;
+
+cout <<"15.Lulus smk"<< endl;
+
+}
+
+void prosedurkuliah()
+
+{
+
+cout <<"16.Umur 18 tahun masuk kuliah"<< endl;
+
+cout <<"17.Semester 1"<< endl;
+
+cout <<"18.Semester 2"<< endl;
+
+cout <<"19.Semester 3"<< endl;
+
+cout <<"20.Semester 4"<< endl;
+
+cout <<"21.Semester 5"<< endl;
+
+cout <<"22.Semester 6"<< endl;
+
+cout <<"23.Semester 7"<< endl;
+
+cout <<"24.Semester 8"<< endl;
+
+cout <<"25.Lulus kuliah dan menjadi sarjana"<< endl;
+
+}
+
