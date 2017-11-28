e064cdf999b8d5a6b386d89cef7f4629d596c329
robyjuniansyah777 <32416484+robyjuniansyah777@users.noreply.github.com>
2017-11-28 21:58:41 +0700

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	
+	int umur;//bilangan
+	string nama;//karakter
+	bool menikah;//true atau fals
+	float ipk;//desimal
+	char goldarah;//karakter
+
+	umur = 8;
+	nama = "roby";
+	menikah = false;
+	ipk = 3,9;
+	goldarah = 'a';
+	
+	
+	cout<<"umur:"<< umur << endl;
+	cout<<"nama :"<< nama << endl;
+	cout<<"menikah :"<< menikah << endl;
+	cout<<"ipk :"<< ipk << endl;
+	cout<<"goldarah :"<< goldarah << endl;
+	
+	return 0;
+	
+}
